fbcac3b7ae1c06c546928ea1670376988c3c39b6
“[firstname <newemail@example.com>
2024-11-30 14:13:35 +0500

    initial commit

---
+++ main.ts
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+let todos : string[] = [];
+let conditon = true;
+
+// Loop until user says no to add more tasks.
+while(conditon){
+let addTask = await inquirer.prompt(
+    [
+        {
+            name: "todo",
+            type: "input",
+            message: "What do you want to add in your todo?"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more Todo?",
+            default: false
+        }
+    ]   
+);
+
+todos.push(addTask.todo);
+conditon = addTask.addMore;
+console.log(todos);
+}
